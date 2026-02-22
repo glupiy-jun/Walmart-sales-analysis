b385c1ea9a5e03f363c9a0e0aa1650d2e8765430
Aleksandr <sassshok228@yandex.ru>
2026-02-22 12:26:38 +0300

    Update Primary analysis.py

--- Primary analysis.py
+++ Primary analysis.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-df = pd.read_csv(r"")
+df = pd.read_csv(r"Walmart_Sales.csv")
 
 print("DataFrame Overview:\n")
 print(df.info())
