4099301939b0b0a7b0c9d346e1a31c5f872c921a
Aleksandr <sassshok228@yandex.ru>
2026-04-25 04:57:07 -0700

    Update visualization.py

--- visualization.py
+++ visualization.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-df = pd.read_csv(r'/Users/aleksandr/Downloads/Walmart_Sales.csv')
+df = pd.read_csv(r'Walmart_Sales.csv')
 
 store_sales = df.groupby('Store')['Weekly_Sales'].sum()
 
